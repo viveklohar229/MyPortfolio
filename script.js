03f4c9e491da8f2085a646c12e5039d739a3016a
viveklohar229 <117832154+viveklohar229@users.noreply.github.com>
2024-08-11 15:03:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.text', {
+    strings: ['Web Development', 'Android Development', 
+        'Java Programming', 'Python'],
+        typeSpeed:100,
+        backSpeed:100,
+        backDelay:1000,
+        loop: true
+  });
